24946ae5300c72e65642f9d3262c9491c1a85ddd
AB244 <ajbenn04@louisville.edu>
2014-10-20 12:06:56 -0400

    Fragment Building

---
+++ app/src/main/java/com/wesleyreisz/mymusic/MyMusicActivityFragment.java
@@ -0,0 +1,59 @@
+package com.wesleyreisz.mymusic;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.wesleyreisz.mymusic.model.Song;
+import com.wesleyreisz.mymusic.service.MockMusicService;
+
+import java.util.List;
+
+
+public class MyMusicActivityFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.activity_my_music_fragment,container,false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ListView listView = (ListView) getView().findViewById(R.id.listViewSongList);
+                List<Song> songs = new MockMusicService().findAll();
+
+        SongAdaptor songAdaptor = new SongAdaptor(this,R.layout.activity_my_music,songs);
+        listView.setAdapter(songAdaptor);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_music, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
