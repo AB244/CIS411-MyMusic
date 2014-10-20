ddc18aae609dc9b802dc639ee677c0bacb78cb41
AB244 <ajbenn04@louisville.edu>
2014-10-20 10:01:50 -0400

    SongAdaptor class created
    Creation of song adaptor class.

--- app/src/main/java/com/wesleyreisz/mymusic/SongAdaptor.java
+++ app/src/main/java/com/wesleyreisz/mymusic/SongAdaptor.java
@@ -1,7 +1,21 @@
 package com.wesleyreisz.mymusic;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.wesleyreisz.mymusic.model.Song;
+
+import java.util.List;
+
 /**
  * Created by Alex on 10/20/2014.
  */
-public class SongAdaptor {
+public class SongAdaptor extends ArrayAdapter<Song>{
+    private Context songContext;
+    private List<Song> listSongEntries;
+    public SongAdaptor(Context context, int textViewResourceId, List<Song> songEntries) {
+        super(context, textViewResourceId, songEntries);
+        songContext = context;
+        listSongEntries = songEntries;
+    }
 }
